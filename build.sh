57cffa42734ecf8df1bae1f90aab79a81a9e0dd5
Auston Stewart <auston@shedbuilt.net>
2020-10-17 10:09:39 -1000

    Update to 60.3 and fix source URL

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # To prevent errors loading shared libraries during installation, comment out LDFLAGSICUDT
-cd source && \
-./configure --prefix=/usr && \
-sed -i "s/LDFLAGSICUDT=.*/LDFLAGSICUDT=/" config/mh-linux && \
-make -j $SHED_NUM_JOBS && \
+cd source &&
+./configure --prefix=/usr &&
+sed -i "s/LDFLAGSICUDT=.*/LDFLAGSICUDT=/" config/mh-linux &&
+make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
